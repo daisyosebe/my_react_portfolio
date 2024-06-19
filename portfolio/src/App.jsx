49bc9781c608a610b65bff9619c4c9ee8fc75a97
daisyosebe <145105156+daisyosebe@users.noreply.github.com>
2024-06-19 16:56:45 -0500

    pages files and components added

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import HomePage from "./pages/homepage/HomePage.jsx";
+import PortfolioPage from "./pages/portfolio/Portfolio.jsx";
+import ContactPage from "./pages/contact/Contact.jsx";
+import ResumePage from "./pages/resume/Resume.jsx";
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+// Exporting functionable component
+export default function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <Header />
+      <div className="pageHeight">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/portfolio" element={<PortfolioPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/resume" element={<ResumePage />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Footer />
+    </BrowserRouter>
   )
-}
-
-export default App
+};
