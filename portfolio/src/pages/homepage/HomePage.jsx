995340369fa456631a34695ec394af1ee9d9badb
daisyosebe <145105156+daisyosebe@users.noreply.github.com>
2024-06-19 21:30:43 -0500

    some css for the contact page, background color, shadow, and txt color

--- portfolio/src/pages/homepage/HomePage.jsx
+++ portfolio/src/pages/homepage/HomePage.jsx
@@ -7,8 +7,18 @@ export default function About() {
     <section className='about'>
       <span className='image'></span>
       <h2 className='about-title'>About Me</h2>
-      <p className='about'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet aliquam id diam maecenas ultricies. Sapien et ligula ullamcorper malesuada proin libero. Felis eget velit aliquet sagittis id consectetur purus.</p>
-      <p className='about'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet aliquam id diam maecenas ultricies. Sapien et ligula ullamcorper malesuada proin libero. Felis eget velit aliquet sagittis id consectetur purus..</p>
+      <p className='about'>
+      My journey into coding began during my college years while I was studying Nursing . Intrigued by the potential of technology, I decided to explore the world of programming languages and software development. Starting with cs50 course, I quickly became engrossed in learning languages like Python and JavaScript. The more I delved into coding, the more fascinated I became by its versatility and power to create solutions. As I am developing simple web applications, Im finding joy in the process of problem-solving through code. With each new skill I acquired, I became increasingly passionate about leveraging technology to tackle pressing world issues.
+      </p>
+
+      <p className='about'> 
+        Originally born in Kenya, I have spent the past 12 years living in the United States. My multicultural upbringing has instilled in me a deep appreciation for diversity and a curiosity to explore different cultures. As a 21-year-old currently studying to become a software engineer, I am dedicated to pursuing my passions while embracing new opportunities for growth and learning.
+      </p>
+
+      <p className='about'>
+        Before embarking on my journey in software engineering, I had aspirations of pursuing a career in nursing, particularly in the neonatal intensive care unit (NICU). As a certified nursing assistant (CNA), I gained valuable experience in patient care and medical assistance. 
+
+      </p>
     </section>
   )
 }
