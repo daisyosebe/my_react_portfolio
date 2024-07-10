cc0efb89d47e4e5a5276be18430e9538d3632bf2
daisyosebe <145105156+daisyosebe@users.noreply.github.com>
2024-07-10 13:25:49 -0500

    Profile pic changed

--- portfolio/src/components/footer/Footer.jsx
+++ portfolio/src/components/footer/Footer.jsx
@@ -11,7 +11,7 @@ export default function Footer(){
           {/* Image link to github */}
           <a href="https://github.com/daisyosebe" target="_blank"><img src="./dist/images/github.JPG" alt="github icon" /></a>
           {/* Image link to linkedin */}
-          <a href="https://www.linkedin.com/in/daisy-osebe/" target="_blank"><img src="./dist/images/linkedin.JPG" alt="linkedin icon" /></a>
+          <a href="https://www.linkedin.com/in/daisy-osebe" target="_blank"><img src="./dist/images/linkedin.JPG" alt="linkedin icon" /></a>
           {/* Image link to facebook */}
           <a href="https://www.facebook.com/daisy.m.osebe/" target="_blank"><img src="./dist/images/facebook.JPG" alt="facebook icon" /></a>
         </div>
